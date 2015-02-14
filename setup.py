c466a7aed3ea04d114355701374bcabc3fdce47a
Ben Jolitz <benjolitz@ucla.edu>
2015-02-14 14:32:22 -0800

    Restructuring/preparing for a modern setuptools world.

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
-import os
-from distutils.core import setup
+from setuptools import (setup, find_packages)
 
-def is_package(path):
-    return (
-        os.path.isdir(path) and
-        os.path.isfile(os.path.join(path, '__init__.py'))
-        )
-
-def find_packages(path, base="" ):
-    """ Find all packages in path """
-    packages = {}
-    for item in os.listdir(path):
-        dir = os.path.join(path, item)
-        if is_package( dir ):
-            if item.lower() == "tests":
-                # Ignore 'tests' packages
-                continue
-            if base:
-                module_name = "%(base)s.%(item)s" % vars()
-            else:
-                module_name = item
-            packages[module_name] = dir
-            packages.update(find_packages(dir, module_name))
-    return packages
-
-setup(name='vbox',
+setup(
+    name='vbox',
     version='0.2.5',
     description="VirtualBox CLI bindings",
     long_description=open("README.rst").read(),
@@ -32,9 +9,9 @@ setup(name='vbox',
     author="Ilja Orlovs",
     author_email="vywn@gryyf.fb".decode("rot13"),
     url='https://github.com/VRGhost/vbox',
-    packages=find_packages("src"),
-    package_dir = {'': 'src'},
-
+    packages=find_packages('src', exclude=['tests']),
+    package_dir={'': 'src'},
+    install_requires=['psutil'],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
